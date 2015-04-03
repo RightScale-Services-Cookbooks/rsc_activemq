071b28a473f9f68ed58f458fdc6cf88d1d9689ae
Stefhen Hovland <stefhen@rightscale.com>
2015-04-03 12:25:18 -0400

    Use rsc_activemq::default instead of activemq::default

--- metadata.rb
+++ metadata.rb
@@ -17,28 +17,28 @@ recipe 'rsc_activemq::default', 'Installs ActiveMQ and sets it up as a service.'
 
 attribute 'activemq/mirror',
   :description => 'URL where to download ActiveMQ tarball.',
-  :recipes     => ['activemq::default'],
+  :recipes     => ['rsc_activemq::default'],
   :type        => 'string',
   :display     => 'activemq/mirror',
   :required    => 'required'
 
 attribute 'activemq/version',
   :description => 'Version of ActiveMQ to be installed.',
-  :recipes     => ['activemq::default'],
+  :recipes     => ['rsc_activemq::default'],
   :type        => 'string',
   :display     => 'activemq/version',
   :required    => 'required'
 
 attribute 'activemq/wrapper/keystore_password',
   :description => 'keystore password',
-  :recipes     => ['activemq::default'],
+  :recipes     => ['rsc_activemq::default'],
   :type        => 'string',
   :display     => 'activemq/wrapper/keystore_password',
   :required    => 'required'
 
 attribute 'activemq/wrapper/truststore_password',
   :description => 'truststore password',
-  :recipes     => ['activemq::default'],
+  :recipes     => ['rsc_activemq::default'],
   :type        => 'string',
   :display     => 'activemq/wrapper/truststore_password',
   :required    => 'required'
